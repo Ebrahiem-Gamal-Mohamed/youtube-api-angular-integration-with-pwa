c08ab3f55dab4f3f99972a9abcf354ed72991522
Ebrahiem-Gamal-Mohamed <e.g.mhmd2020@gmail.com>
2020-05-17 14:35:49 +0200

    add pwa service worker to handle offline status

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppRoutingModule } from "./app-routing.module";
 // components...
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./core/_components/home/home.component";
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 // Modules ...
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   providers: [],
   bootstrap: [AppComponent],
 })
