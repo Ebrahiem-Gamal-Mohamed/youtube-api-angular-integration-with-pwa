a895727c471c3ca56779c9b6c6a77613262a28fb
Ebrahiem-Gamal-Mohamed <e.g.mhmd2020@gmail.com>
2020-05-17 14:58:38 +0200

    add video model

---
+++ src/app/videos/video.model.ts
@@ -0,0 +1,12 @@
+export class Video {
+  id: number;
+  title?: string;
+  description?: string;
+  defaultThumb?: string;
+  publishedDate: string;
+  duration?: string;
+  likeCount?: number;
+  viewsCount?: number;
+  isFavorite?: boolean;
+  rate?: number;
+}
